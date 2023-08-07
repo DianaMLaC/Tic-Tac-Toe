dccef802e2bb1fb542bc6e0068c01b882b59777b
Diana <mada.chirica@gmail.com>
2023-08-07 21:14:59 +0100

    tic-tac-toe-from-sctratch

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('hello world')
+import Game from './game'
+import View from './view'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const el = document.querySelector('figure')
+  if (el === null) {
+    throw new Error('Game HTML element does not exist')
+  }
+  const div = document.querySelector('div')
+  if (div === null) {
+    throw new Error('Game HTML element does not exist')
+  }
+  const result = document.createElement('p')
+  if (result === null) {
+    throw new Error('Game HTML element does not exist')
+  }
+  div.appendChild(result)
+
+  // const player_one = new Player('X')
+  // const player_two = new Player('O')
+  const game = new Game(el, result)
+  // game.play()
+  const view = new View(game, el)
+})
